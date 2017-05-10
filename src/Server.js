9b88e66d97460c6589ef4fc4858793c4817f1208
Maxime C <Maxime@Maxime_C.com>
2017-05-10 15:06:29 +0200

    little maj

--- src/Server.js
+++ src/Server.js
@@ -1,5 +1,5 @@
-import express from 'express';
 import KittensCtrl from './Controllers/KittensCtrl';
+import express from 'express';
 
 export default class Server {
     constructor(port) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Connection from './Connection';
+import Server from './Server';
 import yaml from 'yamljs';
 import path from 'path';
-import Server from './Server';
 
 
 import Kitten from './Kitten';
