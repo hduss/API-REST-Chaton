ec8bdde562215bdaa93a4cc4a6e103455d0f184c
Benoît Ripoche <ripoche.b@gmail.com>
2017-05-11 11:23:11 +0200

    kittens detail

--- _poch/public/js/ajax.js
+++ _poch/public/js/ajax.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //add kitten form
     $('#form').on('submit', (event) => {
         event.preventDefault();
 
@@ -16,7 +17,24 @@ $(document).ready(function() {
             url: '/add',
             data: data,
             success: function(response) {
-                location.reload();
+                //success
+                if (response.errmsg === undefined){
+                    $("#kittens .row").append(
+                        '<div class="col col-lg-2">' +
+                            '<h3>'+response.name+'</h3>' +
+                            '<ul>' +
+                                '<li>Color : '+response.color+'</li>' +
+                                '<li>Quality 1 : '+response.quality1+'</li>' +
+                                '<li>Quality 1 : '+response.quality2+'</li>' +
+                                '<li>Default : '+response.default+'</li>' +
+                                '<li>Best Food : '+response.bestFood+'</li>' +
+                                '<li>Available : '+response.available+'</li>' +
+                            '</ul>' +
+                        '</div>'
+                    );
+                } else {
+                    alert('error : kitten name already taken');
+                }
             },
             error: function(response) {
                 console.log(response);
@@ -24,3 +42,42 @@ $(document).ready(function() {
         });
     });
 });
+
+
+
+//kitten detail
+function more(id){
+    $.ajax({
+        method: 'GET',
+        url: '/kittens/' + id,
+        success: function(response) {
+            //success
+            if (response.errmsg === undefined){
+                console.log(response);
+                $("#kittendetail").html(
+                    `<button onclick="closeDetail()">X</button>
+                    <h2>Kitten Detail</h2>
+                    <h3>${response.name}</h3>
+                    <ul>
+                    <li>Color : ${response.color}</li>
+                    <li>Quality 1 : ${response.quality1}</li>
+                    <li>Quality 1 : ${response.quality2}</li>
+                    <li>Default : ${response.default}</li>
+                    <li>Best Food : ${response.bestFood}</li>
+                    <li>Available : ${response.available}</li>
+                    </ul>`
+                );
+            } else {
+                alert('error !!');
+            }
+        },
+        error: function(response) {
+            console.log(response);
+        }
+    });
+};
+
+//close
+function closeDetail(){
+    $("#kittendetail").html('');
+};
